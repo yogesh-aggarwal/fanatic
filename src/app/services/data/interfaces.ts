b0f56202b9f4f68380ec24e2eb240b6e0133f8f4
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-10-07 15:04:43 +0530

    Added SeriesInterface

--- src/app/services/data/interfaces.ts
+++ src/app/services/data/interfaces.ts
@@ -1,3 +1,25 @@
 export interface PublicTopicsInterface {
   series: string[];
 }
+
+export interface SeriesEpisodeInterface {
+  name: string;
+  description: string;
+  videoId: string;
+  diamonds: string[];
+}
+
+export interface SeriesSeasonInterface {
+  name: string;
+  episodes: SeriesEpisodeInterface[]
+}
+
+export interface SeriesInterface {
+  id: string;
+  name: string;
+  description: string;
+  thumbnail: string;
+  slideshowImages: string[];
+  fans: string[];
+  seasons: SeriesSeasonInterface[]
+}
