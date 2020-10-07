87ac9b8aa1ef6be700a43e553883a636719b8ca1
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-10-07 14:30:16 +0530

    Styled chip

--- src/app/components/ui/chip/chip.component.ts
+++ src/app/components/ui/chip/chip.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'UIChip',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chip.component.scss']
 })
 export class ChipComponent implements OnInit {
+  @Input("isActive")
+  isActive: boolean = false;
 
   constructor() { }
 
