29c6ce3c81cd8ea44e3bd724b712ba424a533624
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-10-07 20:44:44 +0530

    Added animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,41 @@
+import {
+  animate,
+  query,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
 import { Component, OnInit } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 import { DataService } from "./services/data/data.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
+  animations: [
+    trigger("routeAnimation", [
+      transition("* <=> *", [
+        query(":enter, :leave", [
+          style({
+            position: "absolute",
+            opacity: 0,
+            filter: "blur(1px)",
+          }),
+        ]),
+        query(":enter", [animate("300ms ease-in-out", style({ opacity: 1 }))]),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent implements OnInit {
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
     this.dataService.prepareData();
+  }
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet.isActivated ? outlet.activatedRoute : "";
   }
 }
