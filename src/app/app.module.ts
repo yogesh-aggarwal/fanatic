1947db6a7e39d0c3e2b3c22e2a54ba4d99e0ac01
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-10-07 14:11:07 +0530

    Added UIChip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './routes/home/home.component';
-import { LibraryComponent } from './routes/library/library.component';
-import { SeriesModule } from './routes/series/series.module';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./routes/home/home.component";
+import { LibraryComponent } from "./routes/library/library.component";
+import { SeriesModule } from "./routes/series/series.module";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { UiModule } from "./components/ui/ui.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LibraryComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    SeriesModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, SeriesModule, UiModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/ui/ui.module.ts
+++ src/app/components/ui/ui.module.ts
@@ -1,13 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ButtonComponent } from './button/button.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ButtonComponent } from "./button/button.component";
+import { ChipComponent } from "./chip/chip.component";
 
 @NgModule({
-  declarations: [ButtonComponent],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ButtonComponent, ChipComponent],
+  exports: [ButtonComponent, ChipComponent],
+  imports: [CommonModule],
 })
-export class UiModule { }
+export class UiModule {}
