6c88371a209300c49e9ad2ab538dd7565fd30f2a
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-10-08 09:43:38 +0530

    Added cover image to user profile view

--- src/app/routes/user/user.component.ts
+++ src/app/routes/user/user.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UserInterface } from "src/app/services/user/interfaces";
+import { UserService } from "src/app/services/user/user.service";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.scss"],
 })
 export class UserComponent implements OnInit {
+  user: UserInterface;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    UserService.user.subscribe((user) => {
+      this.user = user;
+    });
   }
-
 }
