f776fb9339fd3436c95f03a953738987a4303f69
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-10-14 15:55:42 +0530

    Added loading while funding gems

--- src/app/services/season/season.service.ts
+++ src/app/services/season/season.service.ts
@@ -22,63 +22,65 @@ export class SeasonService {
   async fundGems(amount: number, seriesId: string, seasonId: string) {
     this.dialogService.dialog.next({ type: "loading" });
     this.dialogService.open.next(true);
-    const user: UserInterface = UserService.user.value;
-    const uid: string = user.uid;
-    /// Update Series
-    let series: SeriesInterface = (
-      await this.firestore
-        .collection("series")
-        .doc(seriesId)
-        .snapshotChanges()
-        .pipe(take(1))
-        .toPromise()
-    ).payload.data() as SeriesInterface;
-    let seriesGems: GemsInterface = series.gems;
-    if (seriesGems[uid]) {
-      seriesGems[uid] += amount;
-    } else {
-      seriesGems[uid] = amount;
-    }
-    await this.firestore
-      .collection("series")
-      .doc(seriesId)
-      .update({ gems: seriesGems });
-
-    /// Update Season
-    let season: SeasonInterface = (
-      await this.firestore
-        .collection("series")
-        .doc(seriesId)
-        .collection("seasons")
-        .doc(seasonId)
-        .snapshotChanges()
-        .pipe(take(1))
-        .toPromise()
-    ).payload.data() as SeasonInterface;
-
-    let seasonGems: GemsInterface = season.gems;
-    if (season.id === seasonId) {
-      if (seasonGems[uid]) {
-        seasonGems[uid] += amount;
+    try {
+      const user: UserInterface = UserService.user.value;
+      const uid: string = user.uid;
+      /// Update Series
+      let series: SeriesInterface = (
+        await this.firestore
+          .collection("series")
+          .doc(seriesId)
+          .snapshotChanges()
+          .pipe(take(1))
+          .toPromise()
+      ).payload.data() as SeriesInterface;
+      let seriesGems: GemsInterface = series.gems;
+      if (seriesGems[uid]) {
+        seriesGems[uid] += amount;
       } else {
-        seasonGems[uid] = amount;
+        seriesGems[uid] = amount;
+      }
+      await this.firestore
+        .collection("series")
+        .doc(seriesId)
+        .update({ gems: seriesGems });
+
+      /// Update Season
+      let season: SeasonInterface = (
+        await this.firestore
+          .collection("series")
+          .doc(seriesId)
+          .collection("seasons")
+          .doc(seasonId)
+          .snapshotChanges()
+          .pipe(take(1))
+          .toPromise()
+      ).payload.data() as SeasonInterface;
+
+      let seasonGems: GemsInterface = season.gems;
+      if (season.id === seasonId) {
+        if (seasonGems[uid]) {
+          seasonGems[uid] += amount;
+        } else {
+          seasonGems[uid] = amount;
+        }
+
+        await this.firestore
+          .collection("series")
+          .doc(seriesId)
+          .collection("seasons")
+          .doc(seasonId)
+          .update({ gems: seasonGems });
       }
 
-      await this.firestore
-        .collection("series")
-        .doc(seriesId)
-        .collection("seasons")
-        .doc(seasonId)
-        .update({ gems: seasonGems });
-    }
-
-    /// Update User Balance
-    if (user.gems >= amount) {
-      await this.firestore
-        .collection("users")
-        .doc(uid)
-        .update({ gems: user.gems - amount });
-    }
-    this.dialogService.open.next(true);
+      /// Update User Balance
+      if (user.gems >= amount) {
+        await this.firestore
+          .collection("users")
+          .doc(uid)
+          .update({ gems: user.gems - amount });
+      }
+    } catch {}
+    this.dialogService.open.next(false);
   }
 }
