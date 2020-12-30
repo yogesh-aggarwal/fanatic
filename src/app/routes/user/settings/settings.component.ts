e10c10dedc2bf6fcfd700b5dfbf32a9cc8991f18
Pankaj Aggarwal <pankajaggarwal924@gmail.com>
2020-12-30 13:53:56 +0530

    Added signout button

--- src/app/routes/user/settings/settings.component.ts
+++ src/app/routes/user/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { AuthService } from "src/app/services/auth/auth.service";
 
 @Component({
   selector: "settings",
@@ -6,7 +7,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./settings.component.scss"],
 })
 export class SettingsComponent implements OnInit {
-  constructor() {}
+  constructor(public authService: AuthService) {}
 
   ngOnInit(): void {}
 }
