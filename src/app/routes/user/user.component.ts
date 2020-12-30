31799f7dba69a39f921dd54bfddece00a61c11de
Pankaj Aggarwal <pankajaggarwal924@gmail.com>
2020-12-30 09:55:08 +0530

    Fixed errors while routing in user (/me) page

--- src/app/routes/user/user.component.ts
+++ src/app/routes/user/user.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from "@angular/core";
 import { AnimationsService } from "src/app/animations/animations.service";
 import { UserInterface } from "src/app/services/user/interfaces";
 import { UserService } from "src/app/services/user/user.service";
@@ -13,11 +18,18 @@ export class UserComponent implements OnInit {
   user: UserInterface;
   subRoute: string;
 
-  constructor(public animationService: AnimationsService) {}
+  constructor(
+    public animationService: AnimationsService,
+    private cdRef: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     UserService.user.subscribe((user) => {
       this.user = user;
     });
+  }
+
+  ngAfterViewChecked() {
+    this.cdRef.detectChanges();
   }
 }
