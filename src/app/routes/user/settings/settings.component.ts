46925935a28d78c2a1b62c6a6bddcb520fea6708
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2021-03-01 11:13:38 +0530

    Restructured Settings

--- src/app/routes/user/settings/settings.component.ts
+++ src/app/routes/user/settings/settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 import { AuthService } from "src/app/services/auth/auth.service";
 
 @Component({
@@ -7,7 +8,13 @@ import { AuthService } from "src/app/services/auth/auth.service";
   styleUrls: ["./settings.component.scss"],
 })
 export class SettingsComponent implements OnInit {
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
+
+  async changeAccount() {
+    await this.authService.signOut();
+    await this.authService.signIn();
+    this.router.navigate(["/me"]);
+  }
 }
